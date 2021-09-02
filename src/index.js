f986bb4f6c53a343db5f417637bdb717043226ca
Kirill Korolyov <kirill.korolyov@gmail.com>
2021-09-02 01:12:51 +0100

    Format with prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const canvasSketch = require('canvas-sketch');
-const Color = require('canvas-sketch-util/color');
+const canvasSketch = require("canvas-sketch");
+const Color = require("canvas-sketch-util/color");
 
-const slow = (new URLSearchParams(document.location.search.substring(1))).get('dizzy') === 'false';
+const slow =
+  new URLSearchParams(document.location.search.substring(1)).get("dizzy") ===
+  "false";
 
 const settings = {
-  animate: true
+  animate: true,
 };
 
 const COLOR_CHANGE_SPEED = slow ? 15 : 4; // seconds
@@ -26,47 +28,107 @@ const BOOST_TIME = 6; // seconds
 const RIPPLE_FACTOR = slow ? 0.5 : 1.2;
 
 const drawCircle = (context, width, height, progress, time) => {
-  const radius = Math.sqrt((width) * (width) + (height) * (height)) * progress
-  const radiusBoost = 1 + (GROWTH_BOOST_FACTOR * (1 - progress));
-  const x = width / 2 * (1 + X_CENTER_MOVE_RANGE * Math.sin(time / X_CENTER_MOVE_SPEED * 2 * Math.PI));
-  const y = height / 2 * (1 + Y_CENTER_MOVE_RANGE * Math.sin(time / Y_CENTER_MOVE_SPEED * 2 * Math.PI));
-  const ellipseRatio = (Math.sin(time) / 2 + 0.5) * MIN_ELLIPSE_RATIO + (0.5 - Math.sin(time) / 2) * MAX_ELLIPSE_RATIO; 
-  
-  context.fillStyle = Color.style({ hsl: [ (100 + progress * 200 + time * 360 / COLOR_CHANGE_SPEED) % 360, 100, (Math.sin(time * 2 / COLOR_CHANGE_SPEED) / 2 + 0.5) * 100 ] });
+  const radius = Math.sqrt(width * width + height * height) * progress;
+  const radiusBoost = 1 + GROWTH_BOOST_FACTOR * (1 - progress);
+  const x =
+    (width / 2) *
+    (1 +
+      X_CENTER_MOVE_RANGE *
+        Math.sin((time / X_CENTER_MOVE_SPEED) * 2 * Math.PI));
+  const y =
+    (height / 2) *
+    (1 +
+      Y_CENTER_MOVE_RANGE *
+        Math.sin((time / Y_CENTER_MOVE_SPEED) * 2 * Math.PI));
+  const ellipseRatio =
+    (Math.sin(time) / 2 + 0.5) * MIN_ELLIPSE_RATIO +
+    (0.5 - Math.sin(time) / 2) * MAX_ELLIPSE_RATIO;
+
+  context.fillStyle = Color.style({
+    hsl: [
+      (100 + progress * 200 + (time * 360) / COLOR_CHANGE_SPEED) % 360,
+      100,
+      (Math.sin((time * 2) / COLOR_CHANGE_SPEED) / 2 + 0.5) * 100,
+    ],
+  });
   context.beginPath();
-  context.ellipse(x, y, radius * radiusBoost, radius * radiusBoost * ellipseRatio, time / ROTATION_SPEED * 2 * Math.PI + progress * ROTATION_STAGGERING * 2 * Math.PI, 0, 2 * Math.PI);
+  context.ellipse(
+    x,
+    y,
+    radius * radiusBoost,
+    radius * radiusBoost * ellipseRatio,
+    (time / ROTATION_SPEED) * 2 * Math.PI +
+      progress * ROTATION_STAGGERING * 2 * Math.PI,
+    0,
+    2 * Math.PI
+  );
   context.fill();
-  
+
   context.beginPath();
-  context.strokeStyle = Color.style({ hsl: [ (20 + progress * 200 + time * 360 / COLOR_CHANGE_SPEED) % 360, 0 + 50 + progress * 50, 50 ] });
-  context.lineWidth = (ELLIPSE_MAX_STROKE_WIDTH - ELLIPSE_MIN_STROKE_WIDTH) * progress + ELLIPSE_MIN_STROKE_WIDTH;
-  context.ellipse(x, y, radius * radiusBoost, radius * radiusBoost * ellipseRatio, time / ROTATION_SPEED * 2 * Math.PI + progress * ROTATION_STAGGERING * 2 * Math.PI, 0, 2 * Math.PI);
-  context.fillStyle = Color.style({ hsl: [ (20 + progress * 200 + time * 360 / COLOR_CHANGE_SPEED) % 360, 0 + progress * 100, 50 ] });
+  context.strokeStyle = Color.style({
+    hsl: [
+      (20 + progress * 200 + (time * 360) / COLOR_CHANGE_SPEED) % 360,
+      0 + 50 + progress * 50,
+      50,
+    ],
+  });
+  context.lineWidth =
+    (ELLIPSE_MAX_STROKE_WIDTH - ELLIPSE_MIN_STROKE_WIDTH) * progress +
+    ELLIPSE_MIN_STROKE_WIDTH;
+  context.ellipse(
+    x,
+    y,
+    radius * radiusBoost,
+    radius * radiusBoost * ellipseRatio,
+    (time / ROTATION_SPEED) * 2 * Math.PI +
+      progress * ROTATION_STAGGERING * 2 * Math.PI,
+    0,
+    2 * Math.PI
+  );
+  context.fillStyle = Color.style({
+    hsl: [
+      (20 + progress * 200 + (time * 360) / COLOR_CHANGE_SPEED) % 360,
+      0 + progress * 100,
+      50,
+    ],
+  });
   context.stroke();
-}
+};
 
 const sketch = () => {
   return ({ context, width, height, time }) => {
-    context.fillStyle = 'black';
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
 
-     Array.from({ length: ELLIPSE_COUNT }, (_, i) => {
-      const linearProgress = (((time + (i * ELLIPSE_DURATION / ELLIPSE_COUNT)) % ELLIPSE_DURATION) / ELLIPSE_DURATION);
+    Array.from({ length: ELLIPSE_COUNT }, (_, i) => {
+      const linearProgress =
+        ((time + (i * ELLIPSE_DURATION) / ELLIPSE_COUNT) % ELLIPSE_DURATION) /
+        ELLIPSE_DURATION;
 
       // [0, 1]. values around 0.5 have biggest effect
       const progressBoost = Math.sin(linearProgress * Math.PI);
 
       // [0, 1]
-      const boostProgress = time % (BOOST_DELAY + BOOST_TIME) <= BOOST_DELAY ? 0 : (time % (BOOST_DELAY + BOOST_TIME) - BOOST_DELAY) / BOOST_TIME;
+      const boostProgress =
+        time % (BOOST_DELAY + BOOST_TIME) <= BOOST_DELAY
+          ? 0
+          : ((time % (BOOST_DELAY + BOOST_TIME)) - BOOST_DELAY) / BOOST_TIME;
 
       // 0 when values are completely different, 1 when they are the same
-      const correlation = -Math.cos(Math.abs(progressBoost - boostProgress) * Math.PI)
+      const correlation = -Math.cos(
+        Math.abs(progressBoost - boostProgress) * Math.PI
+      );
 
-      const boostFactor = Math.sin(boostProgress * Math.PI * 2) / 2 * correlation * RIPPLE_FACTOR;
+      const boostFactor =
+        (Math.sin(boostProgress * Math.PI * 2) / 2) *
+        correlation *
+        RIPPLE_FACTOR;
       return linearProgress * (1 + boostFactor);
-    }).sort((a, b) => b - a).forEach(progress => {
-      drawCircle(context, width, height, progress, time);
-    });
+    })
+      .sort((a, b) => b - a)
+      .forEach((progress) => {
+        drawCircle(context, width, height, progress, time);
+      });
   };
 };
 
